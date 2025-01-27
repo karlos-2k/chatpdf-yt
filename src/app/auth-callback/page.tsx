10800c5b8ac59d0f0f56332ce87d1466db911718
Prashant Jhawa <prashantjhawa@gmail.com>
2025-01-27 18:44:44 +0530

    connecting Project

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,13 +1,21 @@
 //  This page is only purpose is to sync log in user and remain in database
 import { useRouter, useSearchParams } from "next/navigation"
+import { trpc } from "../_trpc/client"
 
-const Page = ()=> {
+const Page = async ()=> {
     const router = useRouter()
 
     const searchParams = useSearchParams()
     const origin = searchParams.get('origin')
 
-    const apiResponse = fetch('/api/whatever')
+    const {data, isLoading} = trpc.authCallback.useQuery(undefined, {
+        onSuccess: ({success}: any ) => {
+            if (success){
+                //user is synced to db
+                router.push(origin ? `/${origin}` : '/dashboard')
+            }
+        },
+    });
 }
 
 export default Page
--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,8 +1,29 @@
-import { useState } from "react"
-import { QueryClient} from "@tanstack/react-query"
+import { Children, PropsWithChildren, useState } from "react"
+import { QueryClient, QueryClientProvider} from "@tanstack/react-query"
+import { trpc } from "@/app/_trpc/client"
+import { httpBatchLink } from "@trpc/client"
 
-const Provider = ()=> {
-    const {queryClient} = useState(()=> new queryClient())
+const Provider = ({children}: PropsWithChildren)=> {
+    const [queryClient] = useState(()=> new QueryClient())
+    const [trpcClient] = useState(()=>
+        trpc.createClient({
+            links: [
+                httpBatchLink({
+                    url: 'http://localhost:3000/api/trpc',
+                }),
+            ],
+        })
+    )
+
+    return (
+        <trpc.Provider 
+            client={trpcClient}
+            queryClient={queryClient}>
+            <QueryClientProvider client={queryClient}>
+                {children}
+            </QueryClientProvider>
+        </trpc.Provider>
+    )
 }
 
 export default Provider
