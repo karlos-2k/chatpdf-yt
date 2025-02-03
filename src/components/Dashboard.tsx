d54a12154004b3bf503421d7a832213da69c280e
Prashant Jhawa <prashantjhawa@gmail.com>
2025-02-03 22:39:02 +0530

    uploading file

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -30,8 +30,6 @@ const Dashboard = () => {
         }
     })
 
-    console.log("🚀 Fetched files from API:", files);
-
     return <main className="mx-auto max-w-7xl md:p-10">
         <div className="mt-8 flex flex-col items-start justify-between gap-4 border-b border-gray-200 pb-5 sm:flex-row sm:items-center sm:gap-0">
             <h1 className="mb-3 font-bold text-5xl text-gray-900">
@@ -51,7 +49,7 @@ const Dashboard = () => {
                 ).map((file) => (
                     <li key = {file.id}
                     className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow transition hover:shadow-lg"> 
-                        <Link href={`/dashboard/${file.name}`} className="flex flex-col gap-2">
+                        <Link href={`/dashboard/${file.id}`} className="flex flex-col gap-2">
                             <div className="pt-6 px-6 flex w-full items-center justify-between space-x-6">
                                 <div className="h-10 w-10 flex-shrink-0 rounded-full bg-gradient-to-r from-cyan-500 to-blue-500" />
                                 <div className="flex-1 truncate">
